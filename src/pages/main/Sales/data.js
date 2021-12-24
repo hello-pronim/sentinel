4ca95dea4c6aa1b7630fb77bc3207c260800f73e
mfletcher <prymskiwen@gmail.com>
2021-12-24 13:40:35 -0500

    feat: create company filter dropdown

--- src/pages/main/Sales/data.js
+++ src/pages/main/Sales/data.js
@@ -16,16 +16,22 @@ const data = {
     ],
     data: [
       {
-        label: "Sales ($)",
+        label: "BryBelly",
         values: [
-          2115, 1562, 1584, 1892, 1487, 2223, 2966, 2448, 2905, 3838, 2917,
+          2115, 1562, 1584, 1892, 1087, 2223, 2966, 2448, 2905, 3838, 2917,
           3327,
         ],
       },
       {
-        label: "Orders",
+        label: "Blooming Bath",
         values: [
-          958, 724, 629, 883, 915, 1214, 1476, 1212, 1554, 2128, 1466, 1827,
+          958, 724, 629, 883, 915, 1214, 1476, 1212, 1554, 2128, 466, 1827,
+        ],
+      },
+      {
+        label: "NaturSutten",
+        values: [
+          1214, 883, 1212, 2128, 1554, 958, 724, 915, 1827, 629, 1476, 1466,
         ],
       },
     ],
@@ -35,23 +41,74 @@ const data = {
     {
       id: 1,
       brand: "BryBelly",
-      revenue: "25000000",
-      comparisonRevenue: "19000000",
-      revenueChange: "14",
+      revenue: 25000000,
+      comparisonRevenue: 19000000,
+      revenueChange: 14,
     },
     {
       id: 2,
       brand: "Blooming Bath",
-      revenue: "7000000",
-      comparisonRevenue: "5000000",
-      revenueChange: "6",
+      revenue: 7000000,
+      comparisonRevenue: 5000000,
+      revenueChange: 6,
     },
     {
       id: 3,
       brand: "NaturSutten",
-      revenue: "600000",
-      comparisonRevenue: "300000",
-      revenueChange: "30",
+      revenue: 600000,
+      comparisonRevenue: 300000,
+      revenueChange: 30,
+    },
+  ],
+
+  categories: [
+    { id: 1, name: "Baby" },
+    { id: 2, name: "Novelty" },
+    { id: 3, name: "Pet" },
+    { id: 4, name: "Outdoor" },
+  ],
+  companies: [
+    {
+      category_name: "Baby",
+      company_category_id: 1,
+      id: 2,
+      name: "Blooming Bath",
+      nickname: "Blooming Bath",
+    },
+    {
+      category_name: "Baby",
+      company_category_id: 1,
+      id: 4,
+      name: "Natursutten",
+      nickname: "Natursutten",
+    },
+    {
+      category_name: "Baby",
+      company_category_id: 1,
+      id: 6,
+      name: "Mimijumi",
+      nickname: "Mimijumi",
+    },
+    {
+      category_name: "Novelty",
+      company_category_id: 2,
+      id: 1,
+      name: "Brybelly",
+      nickname: "Brybelly",
+    },
+    {
+      category_name: "Pet",
+      company_category_id: 3,
+      id: 3,
+      name: "Downtown Pet Supply",
+      nickname: "DTPS",
+    },
+    {
+      category_name: "Outdoor",
+      company_category_id: 4,
+      id: 5,
+      name: "Hyke & Byke",
+      nickname: "Hyke & Byke",
     },
   ],
 };
--- src/pages/main/Sales/index.js
+++ src/pages/main/Sales/index.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useStore, useDispatch, useSelector } from "react-redux";
 import styled from "styled-components/macro";
 import { Helmet } from "react-helmet-async";
 
-import { Button, Divider as MuiDivider, Grid, Typography } from "@mui/material";
-import { Add as AddIcon } from "@mui/icons-material";
+import { Divider as MuiDivider, Grid, Typography } from "@mui/material";
 import { spacing } from "@mui/system";
+import {
+  ExpandMore as ExpandMoreIcon,
+  ChevronRight as ChevronRightIcon,
+} from "@mui/icons-material";
 
 import Breadcrumbs from "../../sections/global/Breadcrumbs";
 import SalesTable from "../../sections/SalesTable";
 import async from "../../../components/Async";
+import DropdownButton from "../../../components/button/DropdownButton";
+import TreeViewCheckboxGroup from "../../../components/checkbox/TreeViewCheckboxGroup";
 import data from "./data";
+
+// import {
+//   getCompanies,
+//   selectCompanies,
+//   getCompaniesAsync,
+// } from "../../../redux/slices/company";
 
 const SalesChart = async(() => import("../../sections/SalesChart"));
 
 const Divider = styled(MuiDivider)(spacing);
 
 const Sales = () => {
-  const { salesChartData, brands: brandsTableData } = data;
+  // const dispatch = useDispatch();
+  // const store = useStore();
+  // const companies = useSelector(selectCompanies);
+  // console.log(companies);
+  const {
+    salesChartData,
+    brands: brandsTableData,
+    companies,
+    categories,
+  } = data;
   const brandsTableColumns = [
     { id: "id", label: "#", alignment: "center" },
     { id: "brand", label: "Brand", alignment: "left" },
@@ -29,6 +50,38 @@ const Sales = () => {
     { id: "revenueChange", label: "Revenue Change", alignment: "center" },
     { id: "actions", label: "Actions", alignment: "right" },
   ];
+  const defaultExpandedList = ["0"];
+  const defaultSelectedList = ["0"];
+  const companiesFilterOptions = {
+    id: "0",
+    name: "All",
+    children: categories.map((category, catIndex) => {
+      const sameCatCompanies = companies.filter(
+        (comp) => comp.company_category_id === category.id
+      );
+      defaultExpandedList.push(catIndex + 1 + "0");
+      defaultSelectedList.push(catIndex + 1 + "0");
+
+      return {
+        id: catIndex + 1 + "0",
+        name: category.name,
+        children: sameCatCompanies.map((comp, compIndex) => {
+          defaultSelectedList.push(catIndex + 1 + "" + (compIndex + 1));
+          return {
+            id: catIndex + 1 + "" + (compIndex + 1),
+            name: comp.name,
+          };
+        }),
+      };
+    }),
+  };
+  console.log(defaultExpandedList);
+  console.log(defaultSelectedList);
+
+  // useEffect(() => {
+  //   dispatch(getCompaniesAsync());
+  //   dispatch(getCompanies());
+  // }, []);
 
   return (
     <React.Fragment>
@@ -42,12 +95,24 @@ const Sales = () => {
           <Breadcrumbs pageTitle="Sales" aria-label="Breadcrumb" mt={2} />
         </Grid>
         <Grid item>
-          <div>
-            <Button variant="contained" color="primary">
-              <AddIcon />
-              New Order
-            </Button>
-          </div>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <DropdownButton
+                text="Filter dropdown"
+                variant="contained"
+                color="primary"
+              >
+                <TreeViewCheckboxGroup
+                  defaultCollapseIcon={<ExpandMoreIcon />}
+                  defaultExpandIcon={<ChevronRightIcon />}
+                  data={companiesFilterOptions}
+                  defaultExpanded={defaultExpandedList}
+                  defaultSelected={defaultSelectedList}
+                  multiSelect
+                />
+              </DropdownButton>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
 
