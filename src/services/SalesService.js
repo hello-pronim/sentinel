bc76ba92ddb98cdbc42916d45ff43bafe4b2fb8e
Dan Bunker <mcirque@gmail.com>
2022-02-03 16:22:58 -0700

    more breaking

--- src/services/SalesService.js
+++ src/services/SalesService.js
@@ -1,11 +1,11 @@
 import SecureAxios from "./SecureAxios";
 
 const getSales = (paramsString) => {
-  return SecureAxios.get("/api/sales" + paramsString);
+  return SecureAxios.get("/api" + process.env.API_BASE_URL | "dev" + "/sales" + paramsString);
 };
 
 const getSalesData = (paramsString) => {
-  return SecureAxios.get("/api/sales/data" + paramsString);
+  return SecureAxios.get("/api" + process.env.API_BASE_URL | "dev" + "/sales/data" + paramsString);
 };
 
 export { getSales, getSalesData };
