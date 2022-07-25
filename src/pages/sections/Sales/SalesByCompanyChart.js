739f6139bdac45b8a4fb6831b106abcdf5728d3d
mfletcher <prymskiwen.dev@gmail.com>
2022-07-25 14:10:48 -0500

    feat: load the sales chart per tab

--- src/pages/sections/Sales/SalesByCompanyChart.js
+++ src/pages/sections/Sales/SalesByCompanyChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import styled from "styled-components/macro";
 import Chart from "react-chartjs-2";
 
@@ -13,6 +13,8 @@ import {
 } from "@mui/material";
 import { spacing } from "@mui/system";
 
+import { AuthContext } from "../../../contexts/CognitoContext";
+import { getSalesByCompany } from "../../../services/SalesService";
 import {
   convertMMDDYYYYDateStringToTime,
   convertPriceFormat,
@@ -31,10 +33,15 @@ const LinkText = styled(Typography)`
   cursor: pointer;
 `;
 
-const SalesByCompanyChart = ({ data, loading }) => {
+const SalesByCompanyChart = ({ title }) => {
+  const queryParamsString = window.location.search;
+  const { isInitialized, isAuthenticated, initialize } =
+    useContext(AuthContext);
   const [showCompanyRevenueChart, setShowCompanyRevenueChart] = useState({});
   const [chartData, setChartData] = useState(null);
   const [xAxis, setXAxis] = useState([]);
+  const [salesData, setSalesData] = useState(null);
+  const [loadingSalesData, setLoadingSalesData] = useState(false);
   const options = {
     maintainAspectRatio: false,
     interaction: {
@@ -77,9 +84,35 @@ const SalesByCompanyChart = ({ data, loading }) => {
   };
 
   useEffect(() => {
-    if (data !== null && data.companyRevenueSeries) {
+    initialize();
+  }, [initialize]);
+
+  const refreshSalesData = useCallback(() => {
+    setLoadingSalesData(true);
+    getSalesByCompany(queryParamsString).then((res) => {
+      const { data } = res.data.body;
+
+      setLoadingSalesData(false);
+      if (data) {
+        const _salesData = {
+          companyRevenueSeries: data.company_revenue_series,
+        };
+
+        setSalesData(_salesData);
+      }
+    });
+  }, [queryParamsString]);
+
+  useEffect(() => {
+    if (isAuthenticated && isInitialized) {
+      refreshSalesData();
+    }
+  }, [isInitialized, isAuthenticated, refreshSalesData]);
+
+  useEffect(() => {
+    if (salesData !== null && salesData.companyRevenueSeries) {
       let dates = [];
-      const { companyRevenueSeries } = data;
+      const { companyRevenueSeries } = salesData;
       let newShowCompanyRevenueChart = {};
 
       Object.keys(companyRevenueSeries).forEach((companyId) => {
@@ -117,29 +150,32 @@ const SalesByCompanyChart = ({ data, loading }) => {
         })),
       });
     }
-  }, [data]);
+  }, [salesData]);
 
   useEffect(() => {
-    const { companyRevenueSeries } = data;
-
-    if (companyRevenueSeries) {
-      setChartData({
-        labels: xAxis,
-        datasets: Object.keys(companyRevenueSeries).map((companyId) => ({
-          label: companyRevenueSeries[companyId].name,
-          fill: true,
-          backgroundColor: "transparent",
-          borderColor: getColorFromString(companyRevenueSeries[companyId].name),
-          tension: 0.4,
-          data: xAxis.map((x) =>
-            showCompanyRevenueChart[companyRevenueSeries[companyId].name]
-              ? companyRevenueSeries[companyId].revenue_series[x]
-              : []
-          ),
-        })),
-      });
+    if (salesData !== null) {
+      const { companyRevenueSeries } = salesData;
+      if (companyRevenueSeries) {
+        setChartData({
+          labels: xAxis,
+          datasets: Object.keys(companyRevenueSeries).map((companyId) => ({
+            label: companyRevenueSeries[companyId].name,
+            fill: true,
+            backgroundColor: "transparent",
+            borderColor: getColorFromString(
+              companyRevenueSeries[companyId].name
+            ),
+            tension: 0.4,
+            data: xAxis.map((x) =>
+              showCompanyRevenueChart[companyRevenueSeries[companyId].name]
+                ? companyRevenueSeries[companyId].revenue_series[x]
+                : []
+            ),
+          })),
+        });
+      }
     }
-  }, [data, showCompanyRevenueChart, xAxis]);
+  }, [salesData, showCompanyRevenueChart, xAxis]);
 
   const handleCompanyClicked = (companyName) => {
     let newShowCompanyRevenueChart = { ...showCompanyRevenueChart };
@@ -150,10 +186,10 @@ const SalesByCompanyChart = ({ data, loading }) => {
 
   return (
     <Card variant="outlined" mb={1}>
-      <CardHeader title="Sales by Company" />
+      <CardHeader title={title} />
       <Divider />
       <CardContent>
-        {data !== null && !loading ? (
+        {salesData !== null && !loadingSalesData ? (
           <>
             <Grid container alignItems="center" spacing={2}>
               {Object.keys(showCompanyRevenueChart).map(
