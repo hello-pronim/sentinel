06ea6c7ed7d0ce01071ad5df7e56191b2dfedea8
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-09-28 17:21:53 +0200

    linter: fix linter

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        createProxyMiddleware("/api", {
-            target: "https://2d4hj0r9gf.execute-api.us-east-1.amazonaws.com/dev/",
-            changeOrigin: true,
-            pathRewrite: {
-                "^/api/dev": "",
-            },
-        })
-    );
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "https://2d4hj0r9gf.execute-api.us-east-1.amazonaws.com/dev/",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/dev": "",
+      },
+    })
+  );
 };
